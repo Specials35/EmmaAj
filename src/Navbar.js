ef9c879d75c81f472ff4dee4208d195fdb3412fc
Specials35 <ajkorpon@gmail.com>
2024-03-03 20:16:41 -0500

    Moving nav tags

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,7 @@ const AppNavbar = () => {
             <Container fluid>
                 <Navbar.Brand>Navbar</Navbar.Brand>
 
-                <Nav className="justify-content-start">
+                <Nav className="collapse navbar-collapse">
                     <Nav.Link href="#">Home</Nav.Link>
                     <Nav.Link href="#">About</Nav.Link>
                     <Nav.Link href="#">Contact</Nav.Link>
