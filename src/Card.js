a4167ec0076d90e8d1265db5182150ff69dd1c96
Specials35 <ajkorpon@gmail.com>
2024-03-03 20:00:19 -0500

    Trying to move nav tag more adjusted card content

--- src/Card.js
+++ src/Card.js
@@ -8,7 +8,7 @@ const AppCard = () => {
                 <Card.Body>
                     <Card.Title>Card 1</Card.Title>
                     <Card.Text>
-                        Some quick example text to build on the card.
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
                     </Card.Text>
                 </Card.Body>
             </Card>
@@ -17,7 +17,7 @@ const AppCard = () => {
                 <Card.Body>
                     <Card.Title>Card 2</Card.Title>
                     <Card.Text>
-                        Some quick example text to build on the card.
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
                     </Card.Text>
                 </Card.Body>
             </Card>
@@ -26,7 +26,7 @@ const AppCard = () => {
                 <Card.Body>
                     <Card.Title>Card 3</Card.Title>
                     <Card.Text>
-                        Some quick example text to build on the card.
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
                     </Card.Text>
                 </Card.Body>
             </Card>
--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,7 @@ const AppNavbar = () => {
             <Container fluid>
                 <Navbar.Brand>Navbar</Navbar.Brand>
 
-                <Nav className="ml-auto">
+                <Nav className="ms-auto">
                     <Nav.Link href="#">Home</Nav.Link>
                     <Nav.Link href="#">About</Nav.Link>
                     <Nav.Link href="#">Contact</Nav.Link>
