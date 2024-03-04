2d8b8698ae1a88fa0269b5b3abdd278af73d57e6
Specials35 <ajkorpon@gmail.com>
2024-03-03 19:16:20 -0500

    Bug Fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 import AppNavbar from './Navbar';
 import AppCard from './Card';
 
@@ -7,11 +8,11 @@ function App() {
     return (
         <div className="App">
             <AppNavbar />
-            <AppCard />
             <h1>CS 230L</h1>
             <h2>Section - 002</h2>
             <p>WVU ID: 800375481</p>
             <p>Hi, I am Alexander Korpon</p>
+            <AppCard />
         </div>
     );
 }
