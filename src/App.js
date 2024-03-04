2f0d44a59b79c73431f2f31b28741faf80f96921
Specials35 <ajkorpon@gmail.com>
2024-03-03 19:26:57 -0500

    Attempt to fix website breaking

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import AppNavbar from './Navbar';
--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ import { Card } from 'react-bootstrap';
 const AppCard = () => {
     return (
         <div className="d-flex justify-content-center">
-            <Card style={{width: '18rem', backgroundColor: 'blue', color: 'white'}}>
+            <Card style={{width: '18rem'}}>
                 <Card.Body>
                     <Card.Title>Card 1</Card.Title>
                     <Card.Text>
@@ -13,7 +13,7 @@ const AppCard = () => {
                 </Card.Body>
             </Card>
 
-            <Card style={{width: '18rem', backgroundColor: 'red', color: 'white'}}>
+            <Card style={{width: '18rem'}}>
                 <Card.Body>
                     <Card.Title>Card 2</Card.Title>
                     <Card.Text>
@@ -22,7 +22,7 @@ const AppCard = () => {
                 </Card.Body>
             </Card>
 
-            <Card style={{width: '18rem', backgroundColor: 'green', color: 'white'}}>
+            <Card style={{width: '18rem'}}>
                 <Card.Body>
                     <Card.Title>Card 3</Card.Title>
                     <Card.Text>
